9a9e298d69f3022513ec5bece8514ad39c4c52d4
Kamil <kamiwal92@gmail.com>
2022-04-03 13:09:15 +0200

    add script

---
+++ scr.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_argument("--headless")  # comment it to run with normal way
+
+#driver = webdriver.Chrome(chrome_options=options)  # DeprecationWarning: chrome_options
+driver = webdriver.Chrome(options=options)
+
+driver.get('http://books.toscrape.com/')
+
+# find some elements
+elements = driver.find_elements_by_xpath('//div[@class="side_categories"]//ul//ul//a')
+
+# display elements
+for item in elements:
+    print('category:', item.text)
+
+driver.close()
