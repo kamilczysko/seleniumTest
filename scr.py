17497c2b5844c7ee3d3aaf6ab524ba7dce3d20b1
Kamil Walczak <kamiwal92@gmail.com>
2022-04-03 16:11:17 +0200

    Update scr.py
    do!

--- scr.py
+++ scr.py
@@ -1,7 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
-import chromedriver_binary
 
 options = Options()
 options.add_argument("--headless")  # comment it to run with normal way
